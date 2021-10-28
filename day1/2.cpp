49cc1ab58510e0b8a09e4f19a6e0dc664bc04b5c
AyanDutta <iamayandutta4@gmail.com>
2021-10-28 18:15:42 +0530

    day1 2nd question

---
+++ day1/2.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto it:nums)
+            mp[it]++;
+        for(auto it:mp)
+            if(it.second>=2)
+                return it.first;
+        return 0;
+    }
+};
